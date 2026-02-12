0490a489bfa09a4d4993479c8982bdb431ee404b
Vaishnavi Jha <vaishnavijha0506@gmail.com>
2026-02-13 00:02:31 +0530

    fix: add missing useEffect import

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
